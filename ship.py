09eca9f12ea4754888a2a4e3faa8ec3a04a83622
darknesspaladin <37006242+darknesspaladin@users.noreply.github.com>
2018-07-26 16:34:47 +0800

    飞船左右移动，并且限制移动范围

--- ship.py
+++ ship.py
@@ -2,7 +2,7 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
 
@@ -10,10 +10,13 @@ class Ship():
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
 
         #将每艘新飞船放置在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
 
         # 移动标识
         self.moving_right = False
@@ -21,10 +24,12 @@ class Ship():
 
     def update(self):
         """根据移动标识调整飞船的位置"""
-        if self.moving_right:
-            self.rect.centerx += 2
-        if self.moving_left:
-            self.rect.centerx -= 2
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        #更新rect对象
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船"""
