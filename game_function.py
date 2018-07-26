afd29193613f058e29c45fa493d28e3827f80f22
darknesspaladin <37006242+darknesspaladin@users.noreply.github.com>
2018-07-26 17:05:52 +0800

    重构game_functiong中的check_event（）

--- game_function.py
+++ game_function.py
@@ -2,6 +2,20 @@ import sys
 
 import pygame
 
+def check_keydown_events(event,ship):
+    """按键按下的相应"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    """按键抬起的响应"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """相应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -9,16 +23,11 @@ def check_events(ship):
             sys.exit()
         # 右\左移
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event,ship)
         # 右移停止
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False       
+            check_keyup_events(event,ship)
+     
 def update_screen(ai_settings,screen,bei_jing,ship):
 
     #每次循环时重新汇屏以及背景飞船等内容
